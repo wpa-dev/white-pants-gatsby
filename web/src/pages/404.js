8a68bccab8f9e9d3d5d45a1f51f08c2f6384f266
ntlaseca <nate.tlaseca@gmail.com>
2021-03-12 13:27:47 -0600

    imported 404 page

---
+++ web/src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from 'containers/layout'
+
+const NotFoundPage = () => (
+  <Layout>
+    <Container>
+      <h1>Not found</h1>
+      <p>You just hit a route that doesn&#39;t exist.</p>
+    </Container>
+  </Layout>
+)
+
+export default NotFoundPage
