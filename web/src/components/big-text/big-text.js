7c0f94529b77997e29442115f826758466298cea
ntlaseca <nate.tlaseca@gmail.com>
2021-04-05 12:41:42 -0500

    remove placeholder text

--- web/src/components/big-text/big-text.js
+++ web/src/components/big-text/big-text.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Container, Content } from './big-text.css'
+import BlockContent from 'components/block-content'
 
 const BigText = (props) => (
   <Container color="#000">
     <Content>
-      <h1>We Are White Pants</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <BlockContent blocks={props.blocks}/>
     </Content>
   </Container>
 )
